41fb584abab142b4e0be78fdefbab21126a582c9
devops-timi <tobalereko@gmail.com>
2026-02-25 22:14:39 +0100

    Updated Commit

--- terraform-3tier/modules/rds/main.tf
+++ terraform-3tier/modules/rds/main.tf
@@ -9,27 +9,6 @@ resource "aws_db_subnet_group" "this" {
 }
 
 # ────────────────────────────────────────────
-# Parameter Group (MySQL 8.0)
-# ────────────────────────────────────────────
-resource "aws_db_parameter_group" "this" {
-  name        = "${var.project_name}-mysql8"
-  family      = "mysql8.0"
-  description = "Custom MySQL 8.0 params"
-
-  parameter {
-    name  = "character_set_server"
-    value = "utf8mb4"
-  }
-
-  parameter {
-    name  = "collation_server"
-    value = "utf8mb4_unicode_ci"
-  }
-
-  tags = { Name = "${var.project_name}-mysql8-param-group" }
-}
-
-# ────────────────────────────────────────────
 # Primary RDS Instance (Multi-AZ)
 # ────────────────────────────────────────────
 resource "aws_db_instance" "primary" {
@@ -37,25 +16,22 @@ resource "aws_db_instance" "primary" {
   engine                  = "mysql"
   engine_version          = "8.4"
   instance_class          = var.db_instance_class
-  allocated_storage       = 20
-  max_allocated_storage   = 100         # Enable storage autoscaling up to 100 GB
-  storage_type            = "gp3"
-  storage_encrypted       = true
+  allocated_storage       = 400
+  storage_type            = "gp2"
+  storage_encrypted       = false
 
   db_name  = var.db_name
   username = var.db_username
   password = var.db_password
 
   db_subnet_group_name   = aws_db_subnet_group.this.name
-  parameter_group_name   = aws_db_parameter_group.this.name
   vpc_security_group_ids = [var.db_sg_id]
 
-  multi_az               = true         # Automatic standby in second AZ
+  multi_az               = false         # Automatic standby in second AZ
   publicly_accessible    = false
   deletion_protection    = false        # Set true in production
   skip_final_snapshot    = true         # Set false in production
   backup_retention_period = 7
-  backup_window           = "03:00-04:00"
   maintenance_window      = "Mon:04:00-Mon:05:00"
 
   tags = { Name = "${var.project_name}-mysql-primary" }
@@ -68,7 +44,7 @@ resource "aws_db_instance" "replica" {
   identifier          = "${var.project_name}-mysql-replica"
   replicate_source_db = aws_db_instance.primary.identifier
   instance_class      = var.db_instance_class
-  storage_encrypted   = true
+  storage_encrypted   = false
 
   vpc_security_group_ids = [var.db_sg_id]
   publicly_accessible    = false
